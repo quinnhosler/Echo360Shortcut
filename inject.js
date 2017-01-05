82bf08098275de54b5eef82e60fbcce7e0efc187
Quinn Hosler <qhos93@gmail.com>
2017-01-05 09:10:36 -0500

    new sequcne for FF; implementation of up/down arrows

--- inject.js
+++ inject.js
@@ -4,6 +4,7 @@
 	
 	$('a.classroom-sidebar-close')[0].click()				// close annoying notes bar on right
 	
+	$(document).off('keydown');
 	$(document).on('keydown', function (e) {
 		var button = e.which;
 		
@@ -21,12 +22,17 @@
 			$('.video-volume-btn')[0].click();
 		else if (button == 27)								// escape
 			$('.exitBtn')[0].click();
-		else if (button == 39)								// right
-			$('video')[0].currentTime += 10;		
+		else if (button == 39) {								// right
+			$('video')[0].pause();
+			$('video')[0].currentTime += 10;
+			$('video')[0].play();		
+		}
 		else if (button == 37)								// left
 			$('.video-rewind-btn')[0].click();
-//		else if (button == 38)								// up
-//		else if (button == 40)								// down
+		else if (button == 38)								// up
+			$('.video-speed-btn.plus')[0].click();
+		else if (button == 40)								// down
+			$('.video-speed-btn.minus')[0].click();	
 	});
 
 })();
