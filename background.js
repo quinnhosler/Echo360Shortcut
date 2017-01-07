e4bbc0e943b8d5e637fe13558cab95262e39133a
Quinn Hosler <qhos93@gmail.com>
2017-01-07 15:09:34 -0500

    now only active on echo360 urls

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-    chrome.pageAction.show(tab.id);
-    
-    if (info.status === "complete") {
+    if (tab.url.indexOf('echo360.org') != -1 && info.status === "complete") {
+        chrome.pageAction.show(tab.id);
         chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
             chrome.tabs.executeScript(null, {file: "inject.js"});
         });
+    } else {
+        chrome.pageAction.hide(tab.id);
     }
 });
