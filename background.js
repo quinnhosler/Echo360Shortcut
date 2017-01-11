d18e93d8f77ae4666d8cee29dd8a677fc1260968
Quinn Hosler <qhos93@gmail.com>
2017-01-11 07:50:48 -0500

    colored icon on echo360 page

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
-chrome.tabs.onUpdated.addListener(function(id, info, tab){
-    if (tab.url.indexOf('echo360.org') != -1 && info.status === "complete") {
+chrome.tabs.onUpdated.addListener(function(id, info, tab) {
+    
+    var echo = tab.url.indexOf('echo360.org');
+    if (echo != -1 && info.status === "complete") {
         chrome.pageAction.show(tab.id);
         chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
             chrome.tabs.executeScript(null, {file: "inject.js"});
         });
+    } else if (echo != -1) {
+        chrome.pageAction.show(tab.id);
     } else {
         chrome.pageAction.hide(tab.id);
     }
