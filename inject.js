2ce29e99e7b7b11ae2b53843edffc5c8b4650ddd
Quinn Hosler <qhos93@gmail.com>
2017-01-13 11:08:11 -0500

    shows control bar appropriately, progress bar tracks currentTime

--- inject.js
+++ inject.js
@@ -1,18 +1,18 @@
 // this is the code which will be injected into a given page...
 
 (function() {
-	$('a.disable-btn').hide();
+	$('a.disable-btn').hide();								// eliminates annoying button that disappears the video
 	$(':focus').blur();
-	$('div.content *').on('focus', function(e) {
+	$('div.content *').on('focus', function(e) {			// prevents any element from focusing, maintains consistency
 		$(e.currentTarget).blur();
 	});
 	
-	$('div.media-screen.focused.enabled').on('click', function (e) {
+	$('div.media-screen.focused.enabled').on('click', function (e) {	// this is a stubborn one
 		$(e.currentTarget).removeClass('focus');
 		e.stopImmediatePropagation();
 	})
 	
-	var video = $('video')[0];
+	var video = $('video')[0];								// updates control bar on speed change
 	$('a.video-btn.settings-btn').removeClass('settings-btn').addClass('speed').text(video.playbackRate.toFixed(1)+'x').css('font-size', '1.3em');
 	video.onratechange = function () {
 		$('a.video-btn.speed').text(video.playbackRate.toFixed(1)+'x')
@@ -20,30 +20,34 @@
 	
 	
 	$(document).on('keydown', function (e) {
+
 		var button = e.which;
-		
 		if (button == 75 || button == 32) {					// 'k' or space(32)
 			$('a.video-btn.play-btn')[0].click();
 		}
 		else if (button == 74 || button == 37) {			// 'j' or left arrow
-			if (video.paused == true)
+			if (video.paused == true) {
 				video.currentTime -= 10;
-			else {
+				updateProgress(video)
+			} else {
 				$('a.video-btn.play-btn')[0].click();
 				video.currentTime -= 10;
 				$('a.video-btn.play-btn')[0].click();
 			}
 		}
 		else if (button == 190 || button == 38) {			// Shift+> or up arrow
+			displayControls();
 			video.playbackRate += 0.1;
 		}
 		else if (button == 188 || button == 40)	 {			// Shift+< or down arrow
+			displayControls();
 			video.playbackRate -= 0.1;
 		}
 		else if (button == 39) {							// right arrow
-			if (video.paused == true)
+			if (video.paused == true) {
 				video.currentTime += 10;
-			else {
+				updateProgress(video);
+			} else {
 				$('a.video-btn.play-btn')[0].click();
 				video.currentTime += 10;
 				$('a.video-btn.play-btn')[0].click();
@@ -64,10 +68,45 @@
 			}
 		}
 		else if (button == 77) {							// 'm'
+			displayControls();
 			$('a.video-btn.volume-btn')[0].click();
 		}
 		
 		e.stopImmediatePropagation();
 	});
+	
+	var timer;
+	function displayControls() {
+		if (timer)
+			clearTimeout(timer)
+		
+		$('div.stage.tile').addClass('active');
+		timer = setTimeout(function () {
+			$('div.stage.tile').removeClass('active');
+			timer = null;
+		}, 3000);
+	}
+	
+	
+	function updateProgress(video) {
+		
+		displayControls();
+		$('div.progress').css('width', video.currentTime/video.duration*100+"%");
+		
+		var elapsed;
+		if (video.currentTime >= 3600) {
+			elapsed = pad(video.currentTime/3600)+":"+pad((video.currentTime-3600)/60)+":"+pad(video.currentTime%60);
+		} else {
+			elapsed = pad(video.currentTime/60)+":"+pad(video.currentTime%60);
+		}
+		$('span.currTime span').first().text(elapsed);
+	}
+	
+	function pad(number) {
+		number = parseInt(number)
+		if (number < 10)
+			return "0"+number
+		return number+""
+	}
 })();
 
