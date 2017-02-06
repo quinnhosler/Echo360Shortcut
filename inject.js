9fb3473005ee8fe4221adea4af41be0ed01af57b
Quinn Hosler <qhos93@gmail.com>
2017-02-06 14:06:05 -0500

    will correctly handle multiple videos

--- inject.js
+++ inject.js
@@ -12,10 +12,10 @@
 		e.stopImmediatePropagation();
 	});
 	
-	var video = $('video')[0];								// updates control bar on speed change
-	$('a.video-btn.settings-btn').removeClass('settings-btn').addClass('speed').text(video.playbackRate.toFixed(1)+'x').css('font-size', '1.3em');
-	video.onratechange = function () {
-		$('a.video-btn.speed').text(video.playbackRate.toFixed(1)+'x');
+	var video = $('video');								// updates control bar on speed change
+	$('a.video-btn.settings-btn').removeClass('settings-btn').addClass('speed').text(video[0].playbackRate.toFixed(1)+'x').css('font-size', '1.3em');
+	video[0].onratechange = function () {
+		$('a.video-btn.speed').text(video[0].playbackRate.toFixed(1)+'x');
 	}
 	
 	$(document).on('click', function (e) {
@@ -32,30 +32,57 @@
 			$('a.video-btn.play-btn')[0].click();
 		}
 		else if (button == 74 || button == 37) {			// 'j' or left arrow
-			if (video.paused == true) {
-				video.currentTime -= 10;
-				updateProgress(video);
+			if (video[0].paused == true) {
+				video[0].currentTime -= 10;
+				for (var i = 1; i < video.length; i++) {
+					video[i].currentTime = video[0].currentTime;
+				}
+				updateProgress(video[0]);
 			} else {
+				if (video.length > 1) {
+					alert("An error in Echo360's internal code prevents smooth skipping while 2 videos are playing. Please pause videos and then skip, or click on the progress bar.");
+					return;
+				}
 				$('a.video-btn.play-btn')[0].click();
-				video.currentTime -= 10;
+				video[0].currentTime -= 10;
+				for (var i = 1; i < video.length; i++) {
+					video[i].currentTime = video[0].currentTime;
+				}
 				$('a.video-btn.play-btn')[0].click();
 			}
+			$('a.video-btn.seekBack-btn')[0].click();
 		}
 		else if (button == 190 || button == 38) {			// Shift+> or up arrow
 			displayControls();
-			video.playbackRate += 0.1;
+			video[0].playbackRate += 0.1;
+			for (var i = 1; i < video.length; i++) {
+				video[i].playbackRate = video[0].playbackRate;
+			}
 		}
 		else if (button == 188 || button == 40)	 {			// Shift+< or down arrow
 			displayControls();
-			video.playbackRate -= 0.1;
+			video[0].playbackRate -= 0.1;
+			for (var i = 1; i < video.length; i++) {
+				video[i].playbackRate = video[0].playbackRate;
+			}
 		}
 		else if (button == 39) {							// right arrow
-			if (video.paused == true) {
-				video.currentTime += 10;
-				updateProgress(video);
+			if (video[0].paused == true) {
+				video[0].currentTime += 10;
+				for (var i = 1; i < video.length; i++) {
+					video[i].currentTime = video[0].currentTime;
+				}
+				updateProgress(video[0]);
 			} else {
+				if (video.length > 1) {
+					alert("An error in Echo360's internal code prevents smooth skipping while 2 videos are playing. Please pause videos and then skip, or click on the progress bar.");
+					return;
+				}
 				$('a.video-btn.play-btn')[0].click();
-				video.currentTime += 10;
+				video[0].currentTime += 10;
+				for (var i = 1; i < video.length; i++) {
+					video[i].currentTime = video[0].currentTime;
+				}
 				$('a.video-btn.play-btn')[0].click();
 			}
 		}
